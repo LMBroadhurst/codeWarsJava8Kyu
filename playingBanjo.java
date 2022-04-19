6e7a67dad1684941d696ecc429f2fe297dcb9950
Lewis Broadhurst <lewis1broadhurst@gmail.com>
2022-04-19 22:26:00 +0100

    Create playingBanjo.java

---
+++ playingBanjo.java
@@ -0,0 +1,12 @@
+// Are You Playing Banjo?
+
+public class Banjo {
+  public static String areYouPlayingBanjo(String name) {
+    
+    if( name.toUpperCase().startsWith("R") )
+      return name + " plays banjo";
+    else
+      return name + " does not play banjo";
+    
+  }    
+}
