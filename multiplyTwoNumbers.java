80f41577a48162d2d22efdc1943bb8a554461ba1
Lewis Broadhurst <lewis1broadhurst@gmail.com>
2022-04-20 21:01:49 +0100

    Create multiplyTwoNumbers.java

---
+++ multiplyTwoNumbers.java
@@ -0,0 +1,18 @@
+Function 3 - multiplying two numbers
+
+// Make a function with the class name Kata and the function name multiply that takes in two
+// integers and multiplies them
+
+
+public class Kata {
+  
+  
+  public static int multiply(int n, int m) {
+    
+    int output = n * m;
+    return output;
+    
+  }
+  
+  
+}
