604fa45f6b31c64d095161371f3bf82a8be2f3f5
Lewis Broadhurst <lewis1broadhurst@gmail.com>
2022-04-21 19:50:35 +0100

    Create countTheMonkeys.java

---
+++ countTheMonkeys.java
@@ -0,0 +1,13 @@
+// Count the Monkeys!
+
+public class MonkeyCounter
+{
+  public static int[] monkeyCount(final int n){
+    int[] output = new int[n];
+    
+    for (int i = 0; i < n; i++) {
+      output[i] = i + 1;
+    }
+    return output;
+  }
+}
