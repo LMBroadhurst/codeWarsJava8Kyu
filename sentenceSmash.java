0279f41508519d468017301baf0027d961c756b1
Lewis Broadhurst <lewis1broadhurst@gmail.com>
2022-04-23 19:58:41 +0100

    Create sentenceSmash.java

---
+++ sentenceSmash.java
@@ -0,0 +1,15 @@
+// Sentence Smash
+
+
+import java.util.Arrays;
+
+public class SmashWords {
+
+	public static String smash(String... words) {
+    // TODO Write your code below this comment please
+    
+    String str = String.join(" ", words);
+    return str;
+    
+  }
+}
