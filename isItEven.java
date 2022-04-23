523157b09a742151c2291587be70b45f68eddf20
Lewis Broadhurst <lewis1broadhurst@gmail.com>
2022-04-23 17:48:37 +0100

    Create isItEven.java

---
+++ isItEven.java
@@ -0,0 +1,13 @@
+Is it even?
+
+public class Number {
+
+  public boolean isEven(double n) {
+      // Your awesome code here!
+    if (n % 2 == 0) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+}
