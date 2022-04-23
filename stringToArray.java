af2d03dfe3efcd0e99419b02e46980fa3ce669b7
Lewis Broadhurst <lewis1broadhurst@gmail.com>
2022-04-23 20:02:10 +0100

    Create stringToArray.java

---
+++ stringToArray.java
@@ -0,0 +1,14 @@
+// Convert a string to an array
+
+
+public class Solution {
+
+    public static String[] stringToArray(String s) {
+      
+      String[] arrayOfWords = s.split(" ");
+      
+      return arrayOfWords;
+      
+    }
+
+}
