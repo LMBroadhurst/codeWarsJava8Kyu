a1691bbfdb0342c9cbb1e7d117ebc11d527b5adf
Lewis Broadhurst <lewis1broadhurst@gmail.com>
2022-04-30 11:28:24 +0100

    Create personalisedMessage.java

---
+++ personalisedMessage.java
@@ -0,0 +1,12 @@
+// Grasshopper - Personalized Message
+
+
+class Kata {
+    static String greet(String name, String owner) {
+        if (name.equals(owner)) {
+          return "Hello boss";
+        } else {
+          return "Hello guest";
+        }
+    }
+}
