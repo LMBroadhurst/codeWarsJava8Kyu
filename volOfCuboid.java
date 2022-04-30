5ef8f5969958f232d9f53724827788db608bcc7e
Lewis Broadhurst <lewis1broadhurst@gmail.com>
2022-04-30 14:34:01 +0100

    Create volOfCuboid.java

---
+++ volOfCuboid.java
@@ -0,0 +1,10 @@
+// Volume of a cuboid
+
+public class Kata {
+
+  public static double getVolumeOfCuboid(final double length, final double width, final double height) {
+    
+    return length * width * height;
+  }
+  
+}
