73540a1b02c0af5893b088b09cc8f867cbf7b7e3
Lewis Broadhurst <lewis1broadhurst@gmail.com>
2022-05-13 17:42:27 +0100

    Create getNthEvenNumber.java

---
+++ getNthEvenNumber.java
@@ -0,0 +1,9 @@
+// Get Nth Even Number
+
+
+public class Num {
+  public static int nthEven(int n) {
+  // Can just double the number and then -2 to give the nth even number.
+    return ((n * 2) - 2);
+  }
+}
