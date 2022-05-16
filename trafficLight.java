256ab4529e9b30c3a8cfef34c38ec7130b16c0a8
Lewis Broadhurst <lewis1broadhurst@gmail.com>
2022-05-16 22:23:35 +0100

    Create trafficLight.java

---
+++ trafficLight.java
@@ -0,0 +1,13 @@
+// Thinkful - Logic Drills: Traffic light
+
+public class TrafficLights {
+
+  public static String updateLight(String current) {
+
+    return current.equals("red") ? "green" : current.equals("green") ? "yellow" : "red";
+
+//     First 1 liner :D
+    
+  }
+  
+}
